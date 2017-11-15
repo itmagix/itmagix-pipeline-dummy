165a08a743e637c6f785ce3d4816cb42a39e21b8
Maikel Dollé <maikel@itmagix.nl>
2017-11-15 08:09:42 +0100

    Correct the IP address for the Slave

--- test/spec.js
+++ test/spec.js
@@ -2,13 +2,13 @@
 describe('IT Magix Pipeline Demo', function() {
   it('should have a title', function() {
     browser.waitForAngularEnabled(false);
-    browser.get('http://192.168.2.120:11666');
+    browser.get('http://192.168.2.121:11666');
     expect(browser.getTitle()).toEqual('IT Magix Pipeline Dummy Application');
   });
 
   it('should be able to open pipeline app', function() {
     browser.waitForAngularEnabled(false);
-    browser.get('http://192.168.2.120:11666/pipeline');
+    browser.get('http://192.168.2.121:11666/pipeline');
     expect(browser.getTitle()).toEqual('IT Magix Pipeline Dummy Application');
   });
 });
