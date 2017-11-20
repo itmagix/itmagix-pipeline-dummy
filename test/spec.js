e8a25cfb3dc401cce2457e5e006dd3f73912322c
Maikel Dollé <maikel@itmagix.nl>
2017-11-20 08:57:23 +0100

    Update spec.js

--- test/spec.js
+++ test/spec.js
@@ -3,14 +3,14 @@ describe('IT Magix Pipeline Demo', function() {
   it('should have a title', function() {
     browser.waitForAngularEnabled(false);
     browser.get('http://192.168.2.121:11666');
-    expect(browser.getTitle()).toEqual('IT Magix Pipeline Dummy Application');
     browser.manage().timeouts().pageLoadTimeout(5000);  // 5 seconds
+    expect(browser.getTitle()).toEqual('IT Magix Pipeline Dummy Application');
   });
 
   it('should be able to open pipeline app', function() {
     browser.waitForAngularEnabled(false);
     browser.get('http://192.168.2.121:11666/pipeline');
-    expect(browser.getTitle()).toEqual('IT Magix Pipeline Dummy Application');
     browser.manage().timeouts().pageLoadTimeout(5000);  // 5 seconds
+    expect(browser.getTitle()).toEqual('IT Magix Pipeline Dummy Application');
   });
 });
